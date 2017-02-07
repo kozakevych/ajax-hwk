794c71379b742161fb0318d78ea1313975789cbe
kozakevych <andriikozakevych@gmail.com>
2017-02-07 02:52:57 +0200

    several updates

--- weather-forecast/js/script.js
+++ weather-forecast/js/script.js
@@ -1,21 +1,57 @@
+// clock 
 tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 
 function GetClock(){
-var d=new Date();
-var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
-if(nyear<1000) nyear+=1900;
-var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+	var d=new Date();
+	var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
+	if(nyear<1000) nyear+=1900;
+	var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
 
 
-if(nmin<=9) nmin="0"+nmin;
-if(nsec<=9) nsec="0"+nsec;
+	if(nmin<=9) nmin="0"+nmin;
+	if(nsec<=9) nsec="0"+nsec;
 
-document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+"";
-document.getElementById('time').innerHTML=""+nhour+":"+nmin+":"+nsec+"";
+	document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+"";
+	document.getElementById('time').innerHTML=""+nhour+":"+nmin+":"+nsec+"";
 }
 
 window.onload=function(){
-GetClock();
-setInterval(GetClock,1000);
+	GetClock();
+	setInterval(GetClock,1000);
 }
+
+// getting geolocation and url
+
+/*
+var posLatit, posLongit, urlForecast;
+var secretKey = "884587fb9dfa5db46195850678819907";
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  posLatit = position.coords.latitude;
+  posLongit = position.coords.longitude;
+	urlForecast = "https://api.darksky.net/forecast/" + secretKey + "/" + posLatit +","+ posLongit;
+});
+
+//884587fb9dfa5db46195850678819907
+//https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+
+if (urlForecast) {
+	$.ajax({
+		url: urlForecast,
+		dataType: 'jsonp',
+	})
+	.done(function() {
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+}
+
+
+
+*/
